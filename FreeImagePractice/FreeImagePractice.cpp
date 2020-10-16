e93cbe0e0dc7e9bf749af6eb14734c41651505b1
xogud02 <xogud02@naver.com>
2020-10-16 21:48:25 +0900

    created repos

---
+++ FreeImagePractice/FreeImagePractice.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <FreeImage.h>
+using namespace std;
+
+int main(){
+	auto loaded = FreeImage_Load(FIF_PNG, "C:\\Users\\xogud02\\Desktop\\Monsters\\Aquatic0.png");
+	cout<<FreeImage_GetWidth(loaded);
+}
